805c1f8aefccb51cc457b974e31feabca35c6a47
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2023-05-26 14:17:13 +0000

    Add support for CH430 serial ports

--- telemetrix_rpi_pico/telemetrix_rpi_pico.py
+++ telemetrix_rpi_pico/telemetrix_rpi_pico.py
@@ -306,7 +306,7 @@ class TelemetrixRpiPico(threading.Thread):
         for port in the_ports_list:
             if port.pid is None:
                 continue
-            if port.pid != 10 or port.vid != 11914:
+            if not ((port.pid == 10 and port.vid == 11914) or (port.pid == 29987 and port.vid == 6790)):
                 continue
             try:
                 self.serial_port = serial.Serial(port.device, 115200,
